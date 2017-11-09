85f238250c9324d0a38e9bb91b466da1de75a270
2Jiseong <jsandjo@hanmail.net>
2017-11-09 16:57:05 +0900

    단어 파일 입력 기능 main 및 Makefile 생성

--- src/main.c
+++ src/main.c
@@ -1,1 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void insert(FILE *fp,char eng[10][30],char kor[10][30]);
+
+void main(int argc,char *argv[])
+{
+  char eng[10][30];
+  char kor[10][30];
+  FILE *fp = fopen("word.txt","r");
+  if(fp == NULL)
+  {
+	printf("error!");
+	return;
+  }
+
+  insert(fp,eng,kor);
+
+  for(int i=0;i<5;i++)
+  {
+    printf("%s ",eng[i]);
+    printf("%s ",kor[i]);
+  }
+
+  fclose(fp);
+  return;
+}
