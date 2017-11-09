70320a840b8d8658ae9ca2cb828e2ee3fcec1fe7
2Jiseong <jsandjo@hanmail.net>
2017-11-09 16:13:57 +0900

    단어 파일 입력 기능 라이브러리 함수 추가 및 Makefile 생성

--- lib/insert.c
+++ lib/insert.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+#include<string.h>
+void insert(FILE *fp,char eng[10][30],char kor[10][30])
+{
+   char buf[30];
+   int idx=0;
+   while(fgets(buf,sizeof(buf),fp) != NULL)
+   {
+      char *ptr=strtok(buf," ");
+      strcpy(eng[idx],ptr);
+      ptr = strtok(NULL," ");
+      strcpy(kor[idx],ptr);
+      idx++;
+   }
+
+   return; 
+}
