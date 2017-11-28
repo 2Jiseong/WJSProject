30a8b5e5846e6cf37df6122d9515b6b706bd1fe6
2Jiseong <jsandjo@hanmail.net>
2017-11-28 18:52:46 +0900

    test.c - 난이도별 시험기능 완성

--- lib/test.c
+++ lib/test.c
@@ -1,8 +1,8 @@
-#include <stdio.h>	
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "test.h"
+#include <time.h>
 
 typedef struct word{
         char eng[30];
@@ -12,19 +12,43 @@ typedef struct word{
 
 void test(FILE *fp,char *command)
 {
-        word words[10000];
-        int all_arr[10000]; 
-        int idx = 0;
-	int score = 0;
-	char answer[100] = {0,};
+    word words[10000];
+    int all_arr[10000]; 
+    int idx = 0;
+    int level = 0;
+    int total = 0;
+    int score = 0;
+    char answer[100] = {0,};
 
+	//level 판별
+	if(strcmp(command,"easy") == 0){
+		level = 1;
+	}
+	else if(strcmp(command,"normal")==0){
+		level = 2;
+	}
+	else if(strcmp(command,"hard")==0){
+		level = 3;
+	}
+	else{
+		level = 0;
+	}
+	//단어 읽기
         while (feof(fp) == 0)
         {
-                fscanf(fp, "%s %s %d", words[idx].eng, words[idx].kor, &words[idx].level);
+            fscanf(fp, "%s %s %d", words[idx].eng, words[idx].kor, &words[idx].level);
+	        if(level != words[idx].level)
+		{
+			words[idx].eng[0] = 0;
+			words[idx].kor[0] = 0;
+			words[idx].level = 0;
+			continue;
+		}
 		idx++;
         }
-	
-        for (int i = 0; i < idx; i++)
+	//난수 발생
+	srand(time(0));
+  	for (int i = 0; i < idx; i++)
         {
                 all_arr[i] = rand() % idx;
                 for (int j = 0; j < i; j++)
@@ -36,14 +60,17 @@ void test(FILE *fp,char *command)
                         }
                 }
         }
+	total = idx;
+	idx--;
+	//문제제출 및 정답확인
 	while(1)
         {
-	  if(idx == 0)
-          {
-	     printf("당신의 점수는 %d 입니다!\n",score);
-             break;
-	  }
-          printf("%s: %s: ",words[all_arr[idx]].eng,words[all_arr[idx]].kor);
+ 	  if(idx == -1)
+	  {
+            printf("총 %d 개중 정답개수는 %d개 입니다!\n",total,score);
+	    break;
+ 	  }
+          printf("%s: ",words[all_arr[idx]].eng);
           scanf("%s",answer);
 	  if(strcmp(words[all_arr[idx]].kor,answer) == 0)
           {
@@ -53,7 +80,7 @@ void test(FILE *fp,char *command)
    	  else
 	  {
 	   printf("틀렸습니다\n");
-   	  }
-	  idx--;
+          }
+         idx--;
         }
 }
