80b819ae2cc982d15694ada151de746565127d0e
2Jiseong <jsandjo@hanmail.net>
2017-11-30 17:27:56 +0900

    test가 끝나면 틀린문제 따로 저장하는 기능 추가

--- lib/test.c
+++ lib/test.c
@@ -10,14 +10,16 @@ typedef struct word{
         int level;
 }word;
 
-void test(FILE *fp,char *command)
+void test(FILE *fp,FILE *fp2,char *command)
 {
     word words[10000];
+    word wrongWords[10000];
     int all_arr[10000]; 
     int idx = 0;
     int level = 0;
     int total = 0;
     int score = 0;
+    int fault = 0;
     char answer[100] = {0,};
 
 	//level 판별
@@ -71,24 +73,33 @@ void test(FILE *fp,char *command)
 	total = idx;
 	idx--;
 	//문제제출 및 정답확인
-	while(1)
+        while(1)
         {
- 	  if(idx == -1)
-	  {
+          if(idx == -1)
+          {
             printf("총 %d 개중 정답개수는 %d개 입니다!\n",total,score);
-	    break;
- 	  }
+            for(int i = 0;i<fault;i++) //저장 할 때 기존에 존재하면 cnt++ 없으면 추가하고 cnt = 1
+            {
+             fprintf(fp2,"%s %s %d\n",wrongWords[i].eng,wrongWords[i].kor,wrongWords[i].level);
+            }
+            break;
+          }
           printf("%s: ",words[all_arr[idx]].eng);
           scanf("%s",answer);
-	  if(strcmp(words[all_arr[idx]].kor,answer) == 0)
+          if(strcmp(words[all_arr[idx]].kor,answer) == 0)
           {
-	   score++;
-	   printf("정답입니다!\n");
+           score++;
+           printf("정답입니다!\n");
           }
-   	  else
-	  {
-	   printf("틀렸습니다\n");
+          else
+          {
+           strcpy(wrongWords[fault].eng,words[all_arr[idx]].eng);
+           strcpy(wrongWords[fault].kor,words[all_arr[idx]].kor);
+           wrongWords[fault].level=words[all_arr[idx]].level;
+           fault++;
+           printf("틀렸습니다\n");
           }
          idx--;
         }
+
 }
